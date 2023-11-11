2e4af755083c6e86a3a305379a9f332e21a13554
Apfirebolt <aspper20@gmail.com>
2023-11-12 04:37:56 +0530

    feat: added routing and tailwind

---
+++ client/src/routes/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import NotFound from '../views/NotFound.vue'
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/:catchAll(.*)',
+        name: 'NotFound',
+        component: NotFound
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
---
+++ client/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1 class="text-center text-4xl my-5 text-red-700 font-semibold">Home</h1>
+        <p class="text-red-600">
+            Now add tailwind
+        </p>
+    </div>
+</template>
+
+<script>
+import { reactive, computed } from 'vue';
+
+export default {
+    setup() {
+        
+    },
+};
+</script>
