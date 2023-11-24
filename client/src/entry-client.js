fb4bee7f45b5352d7146e93ee699b3840c75b6de
Apfirebolt <aspper20@gmail.com>
2023-11-24 22:58:37 +0530

    feat: ssr for client side

---
+++ client/src/entry-client.js
@@ -0,0 +1,45 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import VueSmoothScroll from 'vue3-smooth-scroll'
+import './style.css'
+import router from './routes'
+import App from './App.vue'
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+import HeaderComponent from './components/HeaderComponent.vue'
+// Swiper imports
+import SwiperClass, { Pagination } from 'swiper'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/css'
+import 'swiper/css/pagination'
+// AOS imports
+import 'aos/dist/aos.css'
+
+// Use swiper modules
+SwiperClass.use([Pagination])
+
+const app = createApp(App)
+app.use(router)
+app.use(createPinia())
+app.use(VueSmoothScroll)
+app.use(VueAwesomeSwiper)
+app.component('header-component', HeaderComponent)
+app.mount('#app');
+
+const options = {
+    transition: "Vue-Toastification__bounce",
+    maxToasts: 20,
+    newestOnTop: true,
+    position: "top-right",
+    timeout: 5000,
+    closeOnClick: true,
+    pauseOnFocusLoss: true,
+    pauseOnHover: true,
+    showCloseButtonOnHover: false,
+    hideProgressBar: false,
+    closeButton: "button",
+    icon: true,
+    rtl: false,
+};
+
+app.use(Toast, options);
